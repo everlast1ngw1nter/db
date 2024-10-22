062b56db1eac250337522811049ef9409b0393d6
everlast1ngw1nter <at07012004@gmail.com>
2024-10-22 14:44:25 +0500

    full

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using Game.Domain;
+using MongoDB.Driver;
 
 namespace ConsoleApp
 {
@@ -12,8 +13,16 @@ namespace ConsoleApp
 
         private Program(string[] args)
         {
-            userRepo = new InMemoryUserRepository();
-            gameRepo = new InMemoryGameRepository();
+            userRepo = new MongoUserRepository(Create("users"));
+            gameRepo = new MongoGameRepository(Create("users"));
+        }
+        
+        public static IMongoDatabase Create(string databaseName)
+        {
+            var mongoConnectionString = Environment.GetEnvironmentVariable("PROJECT5100_MONGO_CONNECTION_STRING")
+                                        ?? "mongodb://localhost:27017?maxConnecting=100";
+            var mongoClient = new MongoClient(mongoConnectionString);
+            return mongoClient.GetDatabase(databaseName);
         }
 
         public static void Main(string[] args)
